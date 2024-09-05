6acbbd88ee3eb532063267d86b4277cf49977d2a
reenamethar <52745059+reenamethar@users.noreply.github.com>
2024-09-05 23:44:38 +0530

    Exception, Global error handling
    Created custom exception
    Added Global Error Handler

--- employee-directory-api/src/main/java/com/learning/employeedirectory/service/EmployeeServiceImpl.java
+++ employee-directory-api/src/main/java/com/learning/employeedirectory/service/EmployeeServiceImpl.java
@@ -1,11 +1,13 @@
 package com.learning.employeedirectory.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.learning.employeedirectory.dao.EmployeeDAO;
+import com.learning.employeedirectory.errorHandling.EmployeeNotFoundException;
 import com.learning.employeedirectory.pojo.Employee;
 @Component
 public class EmployeeServiceImpl implements EmployeeService{
@@ -19,6 +21,15 @@ public class EmployeeServiceImpl implements EmployeeService{
 	public List<Employee> listEmployees() {
 		// TODO Auto-generated method stub
 		return employeeDAO.findAll();
+	}
+	@Override
+	public Employee findEmployeById(int id) {
+		// TODO Auto-generated method stub
+		Optional<Employee> emp = employeeDAO.findById(id);
+		if(!emp.isPresent()) {
+			throw new EmployeeNotFoundException("Employee not found with employee id "+ id);
+		}
+		return emp.get();
 	}
 
 }
